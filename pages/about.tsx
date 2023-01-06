b128bf0bf7f21a431d427bed497453d588dada23
Anshu Mishra <anshumishra1258@gmail.com>
2023-01-06 12:21:45 +0530

    text

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,7 +14,7 @@ function about() {
         <div id="main">
           
        
-        <h3 className='jt__text text-xl text-green-500 text-bold md:text-5xl text-center md:mx-20 md:pr-20 mt-10 about'>"Unlock the power of Web3 streaming - Anywhere, Anytime"</h3><br></br><br></br>
+        <h3 className='jt__text text-xl text-green-500 text-bold md:text-4xl text-center md:mx-20 md:pr-20 mt-10 about'>"Unlock the power of Web3 streaming - Anywhere, Anytime"</h3><br></br><br></br>
         <p className='text-white text-center mx-4 md:text-3xl md:mx-20 md:pr-20 mt-1'>
 3Ming will be a web3 games streaming platform. Where we will allow peoples to stream web3 games on our streaming platform.
 3MING will help People, Creators and Brands to establish there presence in web3 gaming community and build there own virtual gaming communities.
